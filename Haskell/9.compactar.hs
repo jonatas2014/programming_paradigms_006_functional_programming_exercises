3fb59896f9c4ac28eef94ee8e51b77758d53f28d
Jonatas <jtsdb.snf19@uea.edu.br>
2021-06-12 09:30:14 -0400

    Questão 9 Haskell v1

---
+++ Haskell/9.compactar.hs
@@ -0,0 +1,32 @@
+{-
+recebe uma lista de números e transforma todas as repetições 
+em sub-listas de dois elementos: sendo o primeiro elemento 
+o número de repetições encontradas e o segundo elemento 
+é o número que repete na lista original. 
+Os números que não repetem na lista original não devem ser alterados.
+ex.:
+(compactar '(2 2 2 3 4 4 2 9 5 2 4 5 5 5)) ==> ((3 2) 3 (2 4) 2 9 5 2 4 (3 5))
+-}
+
+-- Função que conta repetições de elementos subsequentes
+repeticoes :: Int -> [Int] -> Int
+repeticoes a [] = 0
+repeticoes a (head:tail) | a == head = repeticoes a tail + 1
+                         | otherwise = 0
+
+-- Coloca no formato da compactação = [repetições subsequentes, número]
+compactar_aux :: Int -> [Int] -> [Int]
+compactar_aux a [] = []
+compactar_aux a (head:tail) | repeticoes a (head:tail) > 1 = [repeticoes a (head:tail), a]
+                            | otherwise = [a]
+
+--Compactação
+compactar :: [Int] -> [[Int]]
+compactar [] = [[]]
+compactar (head:tail) | (repeticoes head (head:tail) > 1) == True =
+                         compactar_aux head (head:tail): 
+                         compactar (drop (repeticoes head (head:tail)) (head:tail))
+                      | otherwise = [head]: compactar tail
+
+
+
